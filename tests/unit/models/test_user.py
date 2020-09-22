acc78cfa2a3920e97f98019b90bfd317ac40a29d
Matt <arddyr@gmail.com>
2020-09-21 21:26:14 -0500

    Added new starter code files for new project.

---
+++ tests/unit/models/test_user.py
@@ -0,0 +1,9 @@
+from section7.starter_code.models.user import UserModel
+from section7.starter_code.tests.unit.unit_base_test import UnitBaseTest
+
+class UserTest(UnitBaseTest):
+    def test_create_user(self):
+        user = UserModel('test', 'abcd')
+
+        self.assertEqual(user.username, 'test')
+        self.assertEqual(user.password, 'abcd')
